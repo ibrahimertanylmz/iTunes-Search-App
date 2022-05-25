9862abcfc071163fe7af2dede66f68201d9e6c15
ibrahimertanylmz <ibrahimertanylmz@gmail.com>
2022-05-25 03:16:17 +0300

    service operations are added

--- app/src/main/java/com/finanskatilim/itunessearchapp/service/TunesApi.kt
+++ app/src/main/java/com/finanskatilim/itunessearchapp/service/TunesApi.kt
@@ -1,12 +1,18 @@
 package com.finanskatilim.itunessearchapp.service
 
 import com.finanskatilim.itunessearchapp.model.SearchResponse
+import com.finanskatilim.itunessearchapp.model.TrackResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface iTunesApi {
+interface TunesApi {
     @GET("search")
     suspend fun getResults(
-        @Query("term") term: String,
+        @Query("term") term: String
     ): SearchResponse
+
+    @GET("lookup")
+    suspend fun getTrackById(
+            @Query("id") id: String
+    ): TrackResponse
 }
--- app/src/main/java/com/finanskatilim/itunessearchapp/service/TunesApiService.kt
+++ app/src/main/java/com/finanskatilim/itunessearchapp/service/TunesApiService.kt
@@ -1,20 +1,25 @@
 package com.finanskatilim.itunessearchapp.service
 
 import com.finanskatilim.itunessearchapp.model.SearchResponse
+import com.finanskatilim.itunessearchapp.model.TrackResponse
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-class iTunesApiService {
+class TunesApiService {
     private val BASE_URL = "https://itunes.apple.com/"
     val api = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .addConverterFactory(GsonConverterFactory.create())
         .build()
-        .create(iTunesApi::class.java)
+        .create(TunesApi::class.java)
 
     suspend fun getResults(query: String): SearchResponse {
         return api.getResults(query)
+    }
+
+    suspend fun getTrackById(query: String): TrackResponse {
+        return api.getTrackById(query)
     }
 }
